f1d09c662b4bd06d32d0a91dc91ef34b203efe59
nevz <projectnevz@gmail.com>
2022-07-30 23:34:18 +0800

    Update auth.ts
    - Minor update on verify route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 import { signIn, signOut, signUp, verify } from '../controllers/authController';
 import isValid from '../middlewares/isValid';
@@ -10,7 +10,12 @@ router.route('/signin').post(validateSignIn(), isValid, signIn);
 
 router.route('/signup').post(validateSignUp(), isValid, signUp);
 
-router.route('/verify').get(verify);
+router.route('/verify').get(verify, (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: 'success',
+    user: req.user
+  });
+});
 
 router.route('/signout').get(signOut);
 export default router;
