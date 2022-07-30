6a534e5189bc8068b910d7515790016035900dbf
nevz <projectnevz@gmail.com>
2022-07-30 23:32:41 +0800

    Update userController.ts
    - Change Author to User
    
    - [x] getAllUserPost
    
    - [x] getAllUnpublishedPost
    
    - [x] getAllPublishedPost
    
    - [x] createUserDraftPost
    
    - [x] createUserPost
    
    - [x] getUserPost
    
    - [x] deleteUserPost
    
    - [x] updateUserPost

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,23 +1,104 @@
 import User from '../model/user';
+import Post from '../model/post';
 import catchAsync from '../utils/catchAsync';
 
+// TODO: Refactor this
+
 // TODO: GET request for all author's post
-export const getAllAuthorPost = catchAsync(async (req, res, next) => {});
+export const getAllUserPost = catchAsync(async (req, res, next) => {
+  const posts = await Post.find({ author: req.params.userId }).exec();
+  return res.status(200).json({
+    status: 'success',
+    posts
+  });
+});
 
 // TODO: GET request for all author's unpublished post
-export const getAllUnpublishedPost = catchAsync(async (req, res, next) => {});
+export const getAllUnpublishedPost = catchAsync(async (req, res, next) => {
+  const posts = await Post.find({
+    author: req.params.userId,
+    published: { $eq: false }
+  }).exec();
+
+  return res.status(200).json({
+    status: 'success',
+    posts
+  });
+});
 
 // TODO: GET request for all author's published post
-export const getAllPublishedPost = catchAsync(async (req, res, next) => {});
+export const getAllPublishedPost = catchAsync(async (req, res, next) => {
+  const posts = await Post.find({
+    author: req.params.userId,
+    published: { $eq: true }
+  }).exec();
+
+  return res.status(200).json({
+    status: 'success',
+    posts
+  });
+});
+
+// TODO: POST request for author's unpublished post
+export const createUserDraftPost = catchAsync(async (req, res, next) => {
+  const { title, body } = req.body;
+  const author = await User.findById(req.params.userId, '-password').exec();
+  const post = await Post.create({
+    author,
+    title,
+    body
+  });
+
+  return res.status(201).json({
+    status: 'success',
+    post
+  });
+});
 
 // TODO: POST request for author's post
-export const createAuthorPost = catchAsync(async (req, res, next) => {});
+export const createUserPost = catchAsync(async (req, res, next) => {
+  const { title, body } = req.body;
+  const author = await User.findById(req.params.userId, '-password').exec();
+  const post = await Post.create({
+    author,
+    title,
+    body,
+    published: true
+  });
+
+  return res.status(201).json({
+    status: 'success',
+    post
+  });
+});
 
 // TODO: GET request for author's post
-export const getAuthorPost = catchAsync(async (req, res, next) => {});
+export const getUserPost = catchAsync(async (req, res, next) => {
+  const post = await Post.findById(req.params.postId).exec();
+
+  return res.status(200).json({
+    status: 'success',
+    post
+  });
+});
 
 // TODO: DELETE request for author's post
-export const deleteAuthorPost = catchAsync(async (req, res, next) => {});
+export const deleteUserPost = catchAsync(async (req, res, next) => {
+  await Post.findByIdAndDelete(req.params.postId).exec();
+
+  return res.status(204).json({
+    status: 'success'
+  });
+});
 
 // TODO: PUT request for author's post
-export const updateAuthorPost = catchAsync(async (req, res, next) => {});
+export const updateUserPost = catchAsync(async (req, res, next) => {
+  const post = await Post.findByIdAndUpdate(req.params.postId, req.body, {
+    new: true
+  }).exec();
+
+  return res.status(200).json({
+    status: 'success',
+    post
+  });
+});
