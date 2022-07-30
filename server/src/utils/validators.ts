78d65de96e43d7b9262b625e624fdbdbb7ef3918
nevz <projectnevz@gmail.com>
2022-07-30 22:11:53 +0800

    Update validators.ts
    - Add validators for signing up

--- server/src/utils/validators.ts
+++ server/src/utils/validators.ts
@@ -11,3 +11,32 @@ export const validateSignIn = function () {
       .withMessage('Password must have at least 8 characters')
   ];
 };
+
+export const validateSignUp = function () {
+  return [
+    check('username')
+      .isLength({ min: 3, max: 15 })
+      .withMessage(
+        'First name must have a min and max length of 3-15 characters'
+      )
+      .matches(/^[A-Za-z\s]+$/)
+      .withMessage('Username must be alphanumeric'),
+    check('email')
+      .isEmail()
+      .withMessage('You have entered an invalid email')
+      .custom((email) =>
+        User.findOne({ email }).then((user) => {
+          if (user) return Promise.reject(new Error('Email is already taken'));
+        })
+      ),
+    check('password')
+      .isLength({
+        min: 8
+      })
+      .withMessage('Password must have at least 8 characters'),
+    check('passwordConfirm', 'Passwords do not match')
+      .isLength({ min: 8 })
+      .withMessage('Password must have at least 8 characters')
+      .custom((value, { req }) => value === req.body.password)
+  ];
+};
