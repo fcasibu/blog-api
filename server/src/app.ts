221191a9e881fc5925ea42f05df53f81811537c3
nevz <projectnevz@gmail.com>
2022-07-30 17:15:30 +0800

    Update app.ts
    - Add Strategy for authentication

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,39 @@
-import createError from "http-errors";
-import express, { NextFunction, Request, Response } from "express";
-import logger from "morgan";
-import flash from "connect-flash";
-import passport from "passport";
-import { Strategy as JWTStrategy } from "passport-jwt";
-import dotenv from "dotenv";
+import createError from 'http-errors';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from 'morgan';
+import passport from 'passport';
+import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 dotenv.config();
 
-import RequestError from "./types/express/";
+import RequestError from './types/express/';
+import User from './model/user';
 
 const app = express();
 
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET_KEY as string
+};
+
+passport.use(
+  new JWTStrategy(options, async (payload, done) => {
+    try {
+      const user = await User.findById(payload.id, '-password')
+        .populate('postCount')
+        .exec();
+      if (!user) return done(null, false, { message: 'User does not exist' });
+
+      return done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
 app.use(passport.initialize());
-app.use(flash());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -26,11 +46,13 @@ app.use(
   (err: RequestError, req: Request, res: Response, next: NextFunction) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+    res.status(err.status || 500).json({
+      status: 'fail',
+      message: err.message
+    });
   }
 );
 
