3778b3ec354c3fb9bd2c2b08a240d9489fb72590
nevz <projectnevz@gmail.com>
2022-07-30 16:00:04 +0800

    Add types for bcryptjs

--- server/src/types/express/index.ts
+++ server/src/types/express/index.ts
@@ -1,3 +1,21 @@
+import mongoose from 'mongoose';
+
 export default interface ResponseError extends Error {
   status?: number;
+}
+
+declare global {
+  namespace Express {
+    interface User {
+      _id: mongoose.Types.ObjectId;
+      firstName: string;
+      lastName: string;
+      email: string;
+      password: string;
+      passowrdConfirm?: string;
+    }
+    interface ResponseError extends Error {
+      status?: number;
+    }
+  }
 }
