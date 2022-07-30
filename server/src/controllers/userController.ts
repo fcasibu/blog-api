3b42c9c4a3998ce965f644eff8b7c1d2c437c65e
nevz <projectnevz@gmail.com>
2022-07-30 22:10:48 +0800

    Add userController.ts

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,23 @@
+import User from '../model/user';
+import catchAsync from '../utils/catchAsync';
+
+// TODO: GET request for all author's post
+export const getAllAuthorPost = catchAsync(async (req, res, next) => {});
+
+// TODO: GET request for all author's unpublished post
+export const getAllUnpublishedPost = catchAsync(async (req, res, next) => {});
+
+// TODO: GET request for all author's published post
+export const getAllPublishedPost = catchAsync(async (req, res, next) => {});
+
+// TODO: POST request for author's post
+export const createAuthorPost = catchAsync(async (req, res, next) => {});
+
+// TODO: GET request for author's post
+export const getAuthorPost = catchAsync(async (req, res, next) => {});
+
+// TODO: DELETE request for author's post
+export const deleteAuthorPost = catchAsync(async (req, res, next) => {});
+
+// TODO: PUT request for author's post
+export const updateAuthorPost = catchAsync(async (req, res, next) => {});
