aaefd9b8e25c53fe7382087e6bf111841048ef88
nevz <projectnevz@gmail.com>
2022-07-30 22:10:41 +0800

    Add postController.ts

---
+++ server/src/controllers/postController.ts
@@ -0,0 +1,11 @@
+import Post from '../model/post';
+import catchAsync from '../utils/catchAsync';
+
+// TODO: Get request for posts
+export const getAllPost = catchAsync(async (req, res, next) => {});
+
+// TODO: GET reuest for post
+export const getPost = catchAsync(async (req, res, next) => {});
+
+// TODO: POST request for comment
+export const createComment = catchAsync(async (req, res, next) => {});
