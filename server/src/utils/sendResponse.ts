8722b8ea77a8db912f1278e6f7fcba69510478a1
nevz <projectnevz@gmail.com>
2022-07-31 20:14:54 +0800

    Update sendResponse.ts
    - Changed interface name from Documents to Data
    
    - Added token in field in interface

--- server/src/utils/sendResponse.ts
+++ server/src/utils/sendResponse.ts
@@ -1,22 +1,22 @@
 import { Response } from 'express';
-import { Model } from 'mongoose';
 import { IComment } from '../model/comment';
 import { IPost } from '../model/post';
 import { IUser } from '../model/user';
 
-interface Documents {
+interface Data {
   posts?: IPost[];
   post?: IPost;
   comments?: IComment[];
   comment?: IComment;
   users?: IUser[];
   user?: IUser;
+  token?: string;
 }
 
-const sendResponse = (res: Response, statusCode: number, doc: Documents) => {
+const sendResponse = (res: Response, statusCode: number, data: Data) => {
   return res.status(statusCode).json({
     status: `${statusCode}`.startsWith('4') ? 'fail' : 'success',
-    ...doc
+    ...data
   });
 };
 
