8516534b0be13c05c9038125de403bf84b3a7396
nevz <projectnevz@gmail.com>
2022-07-31 14:39:04 +0800

    Update index.ts
    - minor formatting of quotes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
-import app from "./app";
+import mongoose from 'mongoose';
+import app from './app';
 
 mongoose
   .connect(process.env.MONGODB as string)
-  .then(() => console.log("Connected to MONGODB"));
+  .then(() => console.log('Connected to MONGODB'));
 
 const db = mongoose.connection;
 
-db.on("error", () => console.error("Failed connecting to MONGODB"));
+db.on('error', () => console.error('Failed connecting to MONGODB'));
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running at https://localhost:${process.env.PORT}`);
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
