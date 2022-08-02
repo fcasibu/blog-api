ee726f7d0aa76140d22d5993f16ba03032a2c1f0
nevz <projectnevz@gmail.com>
2022-08-02 23:54:58 +0800

    Update Form.tsx
    - Added isLoading prop

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 interface FormProps {
   children: React.ReactNode;
   onSubmit(e: React.FormEvent): void;
+  isLoading: boolean;
 }
 
-export default function Form({ children, onSubmit }: FormProps) {
+export default function Form({ children, onSubmit, isLoading }: FormProps) {
   return (
     <form onSubmit={onSubmit}>
       {children}
-      <button type="submit">Submit</button>
+      <button type="submit">{isLoading ? 'Loading...' : 'Submit'}</button>
     </form>
   );
 }
