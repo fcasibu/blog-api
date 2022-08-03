17d5bb13bb0301dd824e60922fb789147b376505
nevz <projectnevz@gmail.com>
2022-08-03 15:36:57 +0800

    Update postController.ts
    - Added query filter for tag
    
    - Added getAllNewPost
    
    - Added getAllTag

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -4,10 +4,17 @@ import catchAsync from '../utils/catchAsync';
 import { paginate } from '../utils/paginate';
 import sendResponse from '../utils/sendResponse';
 
+interface IQuery {
+  published: { $eq: boolean };
+  tag: { $in: string };
+}
+
 // TODO: Get request for posts
 export const getAllPost = catchAsync(async (req, res, next) => {
+  const query = { published: { $eq: true } } as IQuery;
+  if (req.query.tag) query.tag = { $in: req.query.tag as string };
   const skip = paginate(Number(req.query.page ?? 1));
-  const posts = await Post.find({ published: { $eq: true } })
+  const posts = await Post.find(query)
     .skip(skip)
     .limit(10)
     .sort('-createdAt')
@@ -40,4 +47,23 @@ export const createComment = catchAsync(async (req, res, next) => {
   });
 
   return sendResponse(res, 201, { comment });
+});
+
+export const getAllNewPost = catchAsync(async (req, res, next) => {
+  const posts = await Post.find({ published: { $eq: true } })
+    .sort('-createdAt')
+    .limit(3);
+
+  return sendResponse(res, 200, { posts });
+});
+
+export const getAllTag = catchAsync(async (req, res, next) => {
+  const tags = await Post.aggregate([
+    { $match: { published: { $eq: true } } },
+    { $group: { _id: '$tag' } },
+    { $addFields: { tag: '$_id' } },
+    { $project: { _id: 0 } }
+  ]);
+
+  return sendResponse(res, 200, { tags });
 });
