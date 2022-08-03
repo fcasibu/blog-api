aa56e8acec2065829258f028bbbd001e4957b274
nevz <projectnevz@gmail.com>
2022-08-03 21:52:48 +0800

    Update Home.tsx
    - Removed unnecessary code
    
    - Passed necessary props to Posts and Hero Components

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import * as React from 'react';
 import Hero from '../../components/Hero';
 import Posts from '../../components/Posts';
 import useDB from '../../hooks/useDB';
 
 export default function Home() {
-  const { posts } = useDB();
+  const { documents, getFilteredPosts } = useDB();
 
-  React.useEffect(() => {
-    console.log(posts);
-  }, [posts]);
   return (
     <>
-      <Hero />
-      <div>Hello World!</div>
-      <Posts data={posts} />
+      <Hero newPosts={documents.newPosts} />
+      <Posts
+        data={documents.posts}
+        tags={documents.tags}
+        getFilteredPosts={getFilteredPosts}
+      />
     </>
   );
 }
