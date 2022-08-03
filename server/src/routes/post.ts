317b4be564de6a1f35656ac81ea956cf87e2da4c
nevz <projectnevz@gmail.com>
2022-08-03 15:34:39 +0800

    Update post.ts - routes
    - Added routes for /new-posts and /tags

--- server/src/routes/post.ts
+++ server/src/routes/post.ts
@@ -2,13 +2,18 @@ import express from 'express';
 import { verify } from '../controllers/authController';
 import {
   createComment,
+  getAllNewPost,
   getAllPost,
+  getAllTag,
   getPost
 } from '../controllers/postController';
 import isValid from '../middlewares/isValid';
 import { validateComment } from '../utils/validators';
 
 const router = express.Router();
+
+router.route('/new-posts').get(getAllNewPost);
+router.route('/tags').get(getAllTag);
 
 router.route('/').get(getAllPost);
 
