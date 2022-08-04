093bea54bbddadfd34ce3a22248fb3cef4bd5854
nevz <projectnevz@gmail.com>
2022-08-04 22:52:56 +0800

    Update Header.tsx
    - Added a Link tag to SiteLogo
    
    - Added links to Nav
    
    - Need refactoring

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,26 +1,55 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import s from './Header.module.css';
 
 function SiteLogo() {
-  return <div>CodeBrew</div>;
+  return (
+    <div>
+      <Link to="/">CodeBrew</Link>
+    </div>
+  );
 }
 
 function Nav() {
+  const { user, signOut } = useAuth();
+  const navigate = useNavigate();
+
+  const clickHandler = () => {
+    signOut();
+    navigate('/');
+  };
+
+  const links = !user ? (
+    <>
+      <li>
+        <Link to="/signin">Sign in</Link>
+      </li>
+      <li>
+        <Link to="/signup">Sign up</Link>
+      </li>
+    </>
+  ) : (
+    <>
+      <li>
+        <Link to="/cms">CMS</Link>
+      </li>
+      <li>
+        <button type="button" onClick={clickHandler}>
+          Sign Out
+        </button>
+      </li>
+    </>
+  );
+
   return (
     <nav>
-      <ul className={s.links}>
-        <li>
-          <Link to="/signin">Sign in</Link>
-        </li>
-        <li>
-          <Link to="/signup">Sign up</Link>
-        </li>
-      </ul>
+      <ul className={s.links}>{links}</ul>
     </nav>
   );
 }
 
-export default function Header() {
+function Header() {
   return (
     <header className={s.header}>
       <SiteLogo />
@@ -28,3 +57,5 @@ export default function Header() {
     </header>
   );
 }
+
+export default Header;
