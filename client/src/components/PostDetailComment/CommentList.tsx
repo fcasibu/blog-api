351412ea1277957ebe8d996160307352b7a6b046
nevz <projectnevz@gmail.com>
2022-08-04 22:48:21 +0800

    Update CommentList.tsx
    - Added appropriate typing
    
    - Remove unnecessary eslint rule comment

--- client/src/components/PostDetailComment/CommentList.tsx
+++ client/src/components/PostDetailComment/CommentList.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
-import { IComment } from "../../context/DBProvider"
-import { Comment } from "./Comment"
+import { IComment } from '../../context/DBProvider';
+import { Comment } from './Comment';
 
 interface CommentListProps {
-  comments: any
+  comments: IComment[];
 }
 
 export const CommentList = React.memo(({ comments }: CommentListProps) => (
   <>
     {comments.map((comment: IComment) => (
-      /* eslint no-underscore-dangle: 0 */
       <Comment key={comment._id} comment={comment} />
     ))}
   </>
-))
+));
