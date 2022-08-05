1d0144e64b9657413c13bc99a6f2bac6da92f62f
nevz <projectnevz@gmail.com>
2022-08-05 17:25:32 +0800

    Add EditPost Route

--- client/src/routes/cms.tsx
+++ client/src/routes/cms.tsx
@@ -4,14 +4,16 @@ import { Routes, Route } from 'react-router-dom';
 const CMS = React.lazy(() => import('../pages/CMS'));
 const AllPosts = React.lazy(() => import('../pages/CMS/AllPosts'));
 const NewPost = React.lazy(() => import('../pages/CMS/NewPost'));
+const EditPost = React.lazy(() => import('../pages/CMS/EditPost'));
 
 function CMSRoutes() {
   return (
-    <React.Suspense>
+    <React.Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<CMS />}>
           <Route index element={<AllPosts />} />
           <Route path="new-post" element={<NewPost />} />
+          <Route path=":postId/edit" element={<EditPost />} />
         </Route>
       </Routes>
     </React.Suspense>
