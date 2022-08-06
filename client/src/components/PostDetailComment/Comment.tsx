1279028ec22fce3c956051a6858f0ace9fead974
nevz <projectnevz@gmail.com>
2022-08-06 14:54:16 +0800

    Add isAuthor prop to conditionally render a delete button

--- client/src/components/PostDetailComment/Comment.tsx
+++ client/src/components/PostDetailComment/Comment.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import { IComment } from "../../context/DBProvider";
+import React from 'react';
+import { IComment } from '../../context/DBProvider';
 
 export interface CommentProps {
   comment: IComment;
+  isAuthor?: boolean;
 }
 
-export const Comment = React.memo(({ comment }: CommentProps) => (
+export const Comment = React.memo(({ comment, isAuthor }: CommentProps) => (
   <div>
     <h5>{comment.user.username}</h5>
     <p>{comment.text}</p>
     <span>{comment.formattedDate}</span>
-  </div >
+    {isAuthor && (
+      <div>
+        <button type="button">Delete</button>
+      </div>
+    )}
+  </div>
 ));
--- client/src/components/PostDetailComment/CommentList.tsx
+++ client/src/components/PostDetailComment/CommentList.tsx
@@ -4,12 +4,15 @@ import { Comment } from './Comment';
 
 interface CommentListProps {
   comments: IComment[];
+  isAuthor?: boolean;
 }
 
-export const CommentList = React.memo(({ comments }: CommentListProps) => (
-  <>
-    {comments.map((comment: IComment) => (
-      <Comment key={comment._id} comment={comment} />
-    ))}
-  </>
-));
+export const CommentList = React.memo(
+  ({ comments, isAuthor }: CommentListProps) => (
+    <>
+      {comments.map((comment: IComment) => (
+        <Comment key={comment._id} comment={comment} isAuthor={isAuthor} />
+      ))}
+    </>
+  )
+);
