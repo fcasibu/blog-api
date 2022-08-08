2e15fb244949c049c5bf28251fa1e49eabaf7f03
nevz <projectnevz@gmail.com>
2022-08-08 15:43:02 +0800

    Add hero post's images
    - Add isPostOverFiveDays function to check if the post is over five days old

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import { IPost } from '../../context/DBProvider';
 import s from './Hero.module.css';
 
@@ -6,8 +7,11 @@ interface HeroProps {
   newPosts: IPost[];
 }
 
+const isPostOverFiveDays = (post: IPost) => {
+  return  new Date(post.createdAt) <
+  new Date(new Date().getTime() + 5 * 24 * 60 * 60 * 1000);}
+
 function Hero({ newPosts }: HeroProps) {
-  console.log(newPosts);
   return (
     <div className={s.hero}>
       <h1>Read engaging blog posts while you brew</h1>
@@ -16,7 +20,23 @@ function Hero({ newPosts }: HeroProps) {
         reading needs
       </p>
       <button type="button">Check Them Out</button>
-      <div className={s['image-container']}>Hello World!</div>
+      <div className={s['image-container']}>
+        {newPosts.map((post) => (
+          <Link
+            to={`/posts/${post._id}`}
+            key={post._id}
+            className={s['post-image']}
+          >
+            <img src={post.image} alt="" />
+            <div className={s['image-title']}>
+              <p>{post.title}</p>
+            </div>
+            {isPostOverFiveDays(post) && (
+              <span className={s['new-post']}>New</span>
+            )}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
